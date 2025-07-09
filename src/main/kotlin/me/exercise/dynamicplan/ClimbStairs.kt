bc0e55ae199db0a225108e0009917be1c74eb35c
Yeung <caosanyang@gmail.com>
2025-07-09 20:02:57 +0800

    Add Solution 746: minCostClimbingStairs.

--- src/main/kotlin/me/exercise/dynamicplan/ClimbStairs.kt
+++ src/main/kotlin/me/exercise/dynamicplan/ClimbStairs.kt
@@ -24,3 +24,39 @@ fun climb(n: Int): Int {
     }
     return result[n]
 }
+
+/**
+ * 746. 使用最小花费爬楼梯
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。
+ * 一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ * https://leetcode.cn/problems/min-cost-climbing-stairs
+ *
+ * 思路：
+ * 数组 dp[i] 记录到达第 i 个台阶 所需的最小花费
+ * 假设有 n 个台阶，那么 dp 的容量为 n+1，在 n+1 位置记录的是离开第 n 个台阶的花费
+ *
+ * 状态转移方程：dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+ *
+ * 初始状态
+ * dp[0] = 0  // 表示在楼梯下方，还没开始
+ * dp[1] = 0  // 表示从第0或第1个台阶开始，都可以，起点不花费
+ */
+fun minCostClimbingStairs(cost: IntArray): Int {
+    val dp = IntArray(cost.size + 1)
+    dp[0] = 0
+    dp[1] = 0
+    for (i in 2 until dp.size) {
+        dp[i] = minOf(
+            dp[i - 1] + cost[i - 1],
+            dp[i - 2] + cost[i - 2]
+        )
+    }
+    return dp.last()
+}
+
+fun main() {
+    val cost = intArrayOf(10, 15, 20)
+    minCostClimbingStairs(cost).also { println(it) }
+}
---
+++ src/main/kotlin/me/exercise/leetcode/Solution746.kt
@@ -0,0 +1,7 @@
+package me.exercise.leetcode
+
+/**
+ * 标签： 动态规划，递归
+ * https://leetcode.cn/problems/min-cost-climbing-stairs
+ * @see me.exercise.dynamicplan.minCostClimbingStairs
+ */
