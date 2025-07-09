2045e5285537d9156efa5a003dd5f5a5988051e1
Yeung <caosanyang@gmail.com>
2025-07-09 21:20:22 +0800

    Add Java Thread#join demo.

---
+++ src/main/kotlin/me/exercise/concurrent/thread/ThreadJoin.kt
@@ -0,0 +1,21 @@
+package me.exercise.concurrent.thread
+
+val threadA = Thread {
+    repeat(5) {
+        println("${Thread.currentThread().name} $it")
+        Thread.sleep(1000)
+    }
+}
+
+val threadB = Thread {
+    repeat(5) {
+        threadA.join()
+        println("${Thread.currentThread().name} $it")
+        Thread.sleep(1000)
+    }
+}
+
+fun main() {
+    threadA.start()
+    threadB.start()
+}
